c63bdfac937b66e6bdf753f0cbab42f8c41bdddd
1937340563@qq.com <lzqsyc@example>
2025-11-08 02:11:07 +0800

    RTL

--- nvboard/RTL_prj/prj_2/obj_dir/auto_bind.cpp
+++
@@ -1,8 +0,0 @@
-#include <nvboard.h>
-#include "Vtest.h"
-
-void nvboard_bind_all_pins(Vtest* top) {
-	nvboard_bind_pin( &top->a, 1, SW0);
-	nvboard_bind_pin( &top->b, 1, SW1);
-	nvboard_bind_pin( &top->f, 1, LD0);
-}
